cb9a47722f6b25b0dd471f87eee6a9a97075aedd
Ricardo <r.russ77719@gmail.com>
2018-08-02 17:57:12 +0300

    changedFadeOut to simply dissapear

--- js/hideSubscribeBar.js
+++ js/hideSubscribeBar.js
@@ -1,9 +1,9 @@
 jQuery(document).ready(function(){
     jQuery(window).scroll(function() {
         if (jQuery('body').height() <= (jQuery(window).height() + jQuery(window).scrollTop())) {
-            jQuery('#emailBar').fadeOut("slow");
+            jQuery('#emailBar').hide();
         }else{
-            jQuery("#emailBar").fadeIn("slow");
+            jQuery("#emailBar").show();
         }
     });
 });
